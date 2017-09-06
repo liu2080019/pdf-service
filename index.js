c32edd2af14f84096b475af792b6300c9d820cf2
renaesop <njurjs@gmail.com>
2017-09-06 13:38:31 +0800

    适应multipart,处理大表单

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Created by fed on 2017/9/1.
  */
 const koa = require('koa');
-const bodyParser = require('koa-bodyparser');
+const bodyParser = require('koa-body');
 const puppeteer = require('puppeteer');
 
 const app = koa();
@@ -27,14 +27,17 @@ setInterval(() => {
   }
 }, 10 * 60 * 1000);
 
-app.use(bodyParser());
+app.use(bodyParser({
+  multipart: true,
+}));
 
 app.use(function *() {
   const browser = yield getBrowser();
   const page = yield browser.newPage();
   refCount++;
   yield page.goto('about:blank');
-  yield page.setContent(this.request.body.content);
+  const content = this.request.body.content || this.request.body.fields ? this.request.body.fields.content: '';
+  yield page.setContent(content);
   yield page.waitForNavigation({ waitUntil: 'load' })
   this.type = 'application/pdf';
   this.body = yield page.pdf({ format: 'A4' });
