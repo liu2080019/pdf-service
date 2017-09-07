2306360648b15ab447847c2a46dffbf5e1935764
renaesop <njurjs@gmail.com>
2017-09-07 10:17:49 +0800

    增加缓存机制

--- Dockerfile
+++ Dockerfile
@@ -12,4 +12,12 @@ RUN npm i -g pm2
 
 RUN npm i
 
-CMD ["pm2-docker", "-i", "4" , "index.js"]
+ADD crontab /etc/cron.d/hello-cron
+
+# Give execution rights on the cron job
+RUN chmod 0644 /etc/cron.d/hello-cron
+
+
+RUN touch /var/log/cron.log
+
+CMD cron && pm2-docker -i 4 index.js
--- index.js
+++ index.js
@@ -7,12 +7,21 @@ const koa = require('koa');
 const bodyParser = require('koa-body');
 const puppeteer = require('puppeteer');
 
+const router = require('koa-router')();
 const app = koa();
+const read = require('util').promisify(fs.readFile);
+const write = require('util').promisify(fs.writeFile);
 
 let refCount = 0;
 let browser = null;
 
 const defaultContent = fs.readFileSync(path.join(__dirname, 'default.html'), 'utf8');
+const errorContent = fs.readFileSync(path.join(__dirname, 'error.html'), 'utf8');
+const cacheDir = path.join(__dirname, 'cache');
+
+function cacheIdGenerator(content) {
+  return Date.now() + '-' + content.length + '.html';
+}
 
 function getBrowser() {
   if (!browser) {
@@ -36,18 +45,46 @@ app.use(bodyParser({
   multipart: true,
 }));
 
-app.use(function *() {
+function* render(content) {
   const browser = yield getBrowser();
   const page = yield browser.newPage();
   refCount++;
   yield page.goto('about:blank');
-  const content = this.request.body.content || this.request.body.fields ? this.request.body.fields.content: defaultContent;
   yield Promise.all([page.setContent(content), page.waitForNavigation({ waitUntil: 'load' })]);
   this.type = 'application/pdf';
   this.body = yield page.pdf({ format: 'A4' });
   page.close();
   refCount--;
+}
+
+router.post('/', function *(next) {
+  const { no_redirect } = this.request.query;
+  const content = this.request.body.content || this.request.body.fields ? this.request.body.fields.content: defaultContent;
+  if (no_redirect) {
+    yield* render.call(this, content);
+    return;
+  }
+  render.call(this, content)
+  const id = cacheIdGenerator(content);
+  yield write(path.join(cacheDir, id), content);
+  this.redirect('/cache?id=' + id);
+}).get('/', function *(next) {
+  yield* render.call(this, defaultContent);
+}).get('/cache', function* (next) {
+  const { id } = this.request.query;
+  let content;
+  try {
+    content = yield read(path.join(cacheDir, id), 'utf8');
+  } catch (e) {
+    content = errorContent;
+  }
+  yield* render.call(this, content);
 });
+
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 
 app.listen(process.env.PORT || 3000);
